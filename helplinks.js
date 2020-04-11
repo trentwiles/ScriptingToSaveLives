4e18d3f5034ce69320c705ba7428c64f607b3420
Glitch (s2sl) <none>
2020-04-11 14:40:41 +0000

    Added details to the about page

--- helplinks.js
+++ helplinks.js
@@ -1,8 +1,7 @@
 // Add more items here:
 var data = {
   "Support Yale New Haven Hospital " :"https://ynhh.everydayhero.com/us/scripting-to-save-lives",
-  "Folding at home: Dedicate your cpu to help find a coronavirus cure. Try it ":"https://foldingathome.org/",
-  "The world health organization is helping fight the coronavirus. Donate ":"https://www.who.int/emergencies/diseases/novel-coronavirus-2019/donate"
+  "Folding at home. Dedicate your cpu to help find a coronavirus cure. Try it ":"https://foldingathome.org/"
 }
 // https://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
 function randomKey(obj) {
@@ -18,7 +17,7 @@ function loopThrough(){
   $(".take-action").slideUp("fast",function(){
     $(".take-action").html(curKey+" <a style=\"inherit\" class=\"whitetext\"href=\""+curValue+"\">here.</a>");
     $(".take-action").slideDown("slow",function(){
-      setTimeout(loopThrough, 7500)
+      setTimeout(loopThrough, 5000)
     })
     
   })
